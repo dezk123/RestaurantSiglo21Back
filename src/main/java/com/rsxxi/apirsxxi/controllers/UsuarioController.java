f544dc7c13d3378dc1d144bd1e49ae804cfdf7a4
Sebastian Lara <slara@aligare.cl>
2021-09-26 14:30:07 -0300

    Cifrado de contrasena

--- src/main/java/com/rsxxi/apirsxxi/controllers/UsuarioController.java
+++ src/main/java/com/rsxxi/apirsxxi/controllers/UsuarioController.java
@@ -2,6 +2,8 @@ package com.rsxxi.apirsxxi.controllers;
 
 import com.rsxxi.apirsxxi.connection.Conexion;
 import com.rsxxi.apirsxxi.models.Usuario;
+import de.mkammerer.argon2.Argon2;
+import de.mkammerer.argon2.Argon2Factory;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -34,6 +36,10 @@ public class UsuarioController {
 
     @RequestMapping(value = "api/registro", method = RequestMethod.POST)
     public void registrarUsuarios(@RequestBody Usuario usuario) throws SQLException {
+        // Cifrar contrasena
+        Argon2 argon2 = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id);
+        String hash = argon2.hash(1, 1024, 1, usuario.getContrasena());
+        usuario.setContrasena(hash);
         Connection con = configuracion();
         // Llamar al procedimiento que se va a usar
         CallableStatement sp = con.prepareCall("{call SP_REGISTRO(?,?,?,?,?,?,?,?)}");
