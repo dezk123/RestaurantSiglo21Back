3d585b62067fa202cea8aa86f381327382925d9f
Sebastian Lara <slara@aligare.cl>
2021-09-26 12:56:29 -0300

    Conexion

--- src/main/java/com/rsxxi/apirsxxi/connection/Conexion.java
+++ src/main/java/com/rsxxi/apirsxxi/connection/Conexion.java
@@ -1,2 +1,57 @@
-package com.rsxxi.apirsxxi.connection;public class Conexion {
+package com.rsxxi.apirsxxi.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+
+    private String url;
+    private String usuario;
+    private String contrasena;
+
+    public Conexion() {}
+
+    public Conexion(String url, String usuario, String contrasena) {
+        this.url = url;
+        this.usuario = usuario;
+        this.contrasena = contrasena;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Connection obtenerConexion() throws SQLException{
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        Connection con = DriverManager.getConnection(this.url, this.usuario, this.contrasena);
+
+        return con;
+    }
 }
+
