f8603832123227c68e53dd4bb706dfaa282c71e3
Sebastian Lara <slara@aligare.cl>
2021-09-26 12:46:24 -0300

    Inicio de proyecto

---
+++ src/main/java/com/rsxxi/apirsxxi/ApirsxxiApplication.java
@@ -0,0 +1,13 @@
+package com.rsxxi.apirsxxi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirsxxiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApirsxxiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rsxxi/apirsxxi/ApirsxxiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rsxxi.apirsxxi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirsxxiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
