dc56a2dff758f8a9021df2398926506ce106632c
Sebastian Lara <slara@aligare.cl>
2021-09-27 15:23:08 -0300

    Autenticacion controller

---
+++ src/main/java/com/rsxxi/apirsxxi/controllers/AutenticacionController.java
@@ -0,0 +1,52 @@
+package com.rsxxi.apirsxxi.controllers;
+
+import com.rsxxi.apirsxxi.connection.Conexion;
+import com.rsxxi.apirsxxi.models.Usuario;
+import de.mkammerer.argon2.Argon2;
+import de.mkammerer.argon2.Argon2Factory;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.*;
+
+@RestController
+public class AutenticacionController {
+
+    private Connection configuracion() throws SQLException {
+        Conexion con = new Conexion(
+                "jdbc:oracle:thin:@localhost:1521:XE",
+                "RSXXI",
+                "rsxxi"
+        );
+        return con.obtenerConexion();
+    }
+
+    @RequestMapping(value = "api/login", method = RequestMethod.POST)
+    public Usuario login(@RequestBody Usuario usuario) throws SQLException {
+        System.out.println(usuario.getIdUsuario());
+        Connection con = configuracion();
+        Statement st = con.createStatement();
+        // Generar query
+        String query = String.format("SELECT * FROM USUARIO WHERE IDUSUARIO = %d", usuario.getIdUsuario());
+        ResultSet res = st.executeQuery(query);
+        // comprobar usuario
+        Usuario aux = new Usuario();
+        while (res.next()){
+            aux.setIdUsuario(res.getInt(1));
+            aux.setIdTipoUsuario(res.getString(2));
+            aux.setCorreo(res.getString(3));
+            aux.setContrasena(res.getString(4));
+            aux.setNombre(res.getString(5));
+            aux.setApellido(res.getString(6));
+            aux.setDireccion(res.getString(7));
+            aux.setRun(res.getString(8));
+        }
+        Argon2 argon2 = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id);
+        if(argon2.verify(aux.getContrasena(), usuario.getContrasena())){
+            return aux;
+        }
+        return null;
+    }
+}
