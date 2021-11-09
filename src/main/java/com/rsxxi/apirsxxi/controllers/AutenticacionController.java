11a5b8d42afcf17adcbe95b5e0a80a45f13dee84
Sebastian Lara <slara@aligare.cl>
2021-11-09 16:39:38 -0300

    Login

--- src/main/java/com/rsxxi/apirsxxi/controllers/AutenticacionController.java
+++ src/main/java/com/rsxxi/apirsxxi/controllers/AutenticacionController.java
@@ -1,7 +1,7 @@
 package com.rsxxi.apirsxxi.controllers;
 
 import com.rsxxi.apirsxxi.connection.Conexion;
-import com.rsxxi.apirsxxi.models.Aux;
+import com.rsxxi.apirsxxi.models.User;
 import com.rsxxi.apirsxxi.models.Login;
 import com.rsxxi.apirsxxi.models.Usuario;
 import com.rsxxi.apirsxxi.utils.JWTUtil;
@@ -25,15 +25,15 @@ public class AutenticacionController {
   private JWTUtil jwtUtil;
 
   @RequestMapping(value = "api/login", method = RequestMethod.POST)
-  public Aux login(@RequestBody Login login) throws SQLException {
+  public User login(@RequestBody Login login) throws SQLException {
     Connection con = configuracion();
     Usuario aux = new Usuario();
     aux = aux.buscarUsuario(con, login.getCorreo());
     Argon2 argon2 = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id);
     if(argon2.verify(aux.getContrasena(), login.getContrasena())){
       // retornar token
-      Aux aux1 = new Aux(aux.getNombre(), jwtUtil.create(aux.getCorreo(), aux.getIdTipoUsuario()));
-      return aux1;
+      User user = new User(aux.getNombre(), jwtUtil.create(aux.getCorreo(), aux.getIdTipoUsuario()));
+      return user;
     }
     return null;
   }
--- src/main/java/com/rsxxi/apirsxxi/models/User.java
+++ src/main/java/com/rsxxi/apirsxxi/models/User.java
@@ -3,14 +3,14 @@ package com.rsxxi.apirsxxi.models;
 import lombok.Getter;
 import lombok.Setter;
 
-public class Aux {
+public class User {
   @Getter @Setter
   private String nombre;
 
   @Getter @Setter
   private String token;
 
-  public Aux(String nombre, String token) {
+  public User(String nombre, String token) {
     this.nombre = nombre;
     this.token = token;
   }
